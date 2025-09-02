f308063afbbf15c40a2b1dc8503de35253377a38
Luis Santana <angelsantana20102@outlook.com>
2025-09-02 08:55:33 -0600

    bank balance Luis Santana

---
+++ spark/notebooks/lib/luis_santana/bank.py
@@ -0,0 +1,9 @@
+# Problem Statement
+# Create a BankAccount class and add it in your own module 1 that uses a
+# dictionary to store operations (functions) as values and their names (e.g.,
+# "deposit", "withdraw", and "get balance") as keys. Dynamically select and
+# execute operations based on user input.
+# Example Usage
+# account = BankAccount(1000)
+# account.process("deposit", 13)
+# account.process("withdraw", 717)
