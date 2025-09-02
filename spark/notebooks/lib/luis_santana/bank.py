48bf3f8d0c270f7a55535dea2e3c1ffb4029f3b8
Luis Santana <angelsantana20102@outlook.com>
2025-09-02 10:06:48 -0600

    Bank class and examples

--- spark/notebooks/lib/luis_santana/bank.py
+++ spark/notebooks/lib/luis_santana/bank.py
@@ -7,3 +7,44 @@
 # account = BankAccount(1000)
 # account.process("deposit", 13)
 # account.process("withdraw", 717)
+
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        if(initial_balance < 0):
+            raise ValueError("Initial balance canot be negative")
+        self.balance = initial_balance
+        self.dict = {
+            "deposit": self.deposit,
+            "withdraw": self.withdraw,
+            "get_balance": self.get_balance
+        }
+
+    def deposit(self, amount):
+        if (amount > 0):
+            self.balance += amount
+            return f"Deposited: {amount}, New Balanec: {self.balance}"
+        else:
+            raise ValueError("Deposit amount must be positive")
+
+    def withdraw(self, amount):
+        if(amount > self.balance):
+            raise ValueError("Insufficient funds")
+        if (amount <= 0):
+            raise ValueError("Withdrawal amount must be positive")
+        self.balance -= amount
+        return f"Withdrew: {amount}, New Balanec: {self.balance}"
+
+    def get_balance(self):
+        return self.balance
+    
+    def process(self, operation, amount=0):        
+        if(operation in self.dict):
+            if(operation) == "get_balance":
+                return self.dict[operation]()
+            else:
+                return self.dict[operation](amount)
+        else:
+            raise ValueError("Invalid operation")
+    
+    def __repr__(self):
+        return f"BankAccount(balance={self.balance})"
