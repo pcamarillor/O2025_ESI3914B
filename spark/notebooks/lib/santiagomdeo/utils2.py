2b81258f8a7d200d4b1a133860d75f4d9ca6d21f
Santiago Montes de Oca <smontesdeoca.-@iteso.mx>
2025-09-02 09:03:40 -0600

    ...

--- spark/notebooks/lib/santiagomdeo/utils2.py
+++ spark/notebooks/lib/santiagomdeo/utils2.py
@@ -1,21 +1,29 @@
+
+
+def deposit(money, quantity_added):
+    return money + quantity_added
+
+def withdraw(money, quantity_to_withdraw):
+    if money > quantity_to_withdraw:
+        return money - quantity_to_withdraw
+    else:
+        raise ValueError("you are kinda broke (not enough funds)")
+
+
+
 class BankAccount:
     def __init__(self, initial_money):
-        self.money = initial_money
-
-    @staticmethod
-    def deposit(money, quantity_added):
-        return money + quantity_added
-    @staticmethod
-    def withdraw(money, quantity_to_withdraw):
-        if money > quantity_to_withdraw:
-            return money - quantity_to_withdraw
-        else:
-            raise ValueError("you are kinda broke (not enough funds)")
-        
-    operations = {
-        'deposit': deposit,
-        'withdraw': withdraw
-    }
+        self.__money = initial_money
+        self.operations = {
+            'deposit': deposit,
+            'withdraw': withdraw
+        }
+    
+    @property
+    def money(self):
+        return self.__money
+    
 
     def process(self, func, quantity):
-        self.money = operations[func]
+        self.__money = self.operations[func](self.__money, quantity)
+        return self.__money 
