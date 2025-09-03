213fa1eb7571617f56288f4d5e10c01997cc0e52
Valeria-Oliva <valeriaoliva@iteso.mx>
2025-09-03 17:12:36 -0600

    Lab02 Valeria Oliva

--- spark/notebooks/lib/valeriaoliva/utils.py
+++ spark/notebooks/lib/valeriaoliva/utils.py
@@ -1,123 +1,29 @@
 class BankAccount:
-  def __init__(self, balance):
-      balance = 0
-      self.update_balance = balance
-      
-      self.title = title
-      self.author = author
-
-  def __str__(self):
-      return f"{self.title} by {self.author}"
-
-  def __repr__(self):
-      return f"Book('{self.title}', '{self.author}')"
-  
-class MathUtils:
-  @staticmethod
-  def add(x, y):
-      return x + y
-  
-def add2(x,y):
-    return x + y
-
-def add(*args):
-    total = 0 
-    for n in args:
-        total += n
-    return total
-
-def substract(*args):
-    total = 0 
-    for n in args:
-        total -= n
-    return total
-
-def substract2 (x,y):
-    return x - y
+    def __init__(self, balance):
+        self.update_balance = balance
+        self.dict_operations = {
+            'deposit': self.deposit,
+            'withdraw' : self.withdraw,
+            'get_balance' : self.get_balance}
     
-
-class Dog:
-  species = "Canis familiaris"
-
-  @classmethod
-  def get_species(cls):
-      return cls.species
-
-# Base class
-class Figure:
-    def __init__(self, name):
-        self.name = name
     
-    def area(self):
-        raise NotImplementedError("Subclasses must implement this method")
+    def get_balance(self):
+        return f"* Su balance es: $ {self.update_balance}"
     
-    def perimeter(self):
-        raise NotImplementedError("Subclasses must implement this method")
     
-    def display(self):
-        print(f"Figure: {self.name}")
-        print(f"Area: {self.area()}")
-        print(f"Perimeter: {self.perimeter()}")
-
-# Inherited class for Circle
-class Circle(Figure):
-    def __init__(self, radius):
-        super().__init__("Circle")
-        self._radius = radius
+    def deposit(self,value):
+        self.update_balance += value
+        return f"* Su depósito de $ {value} ha sido realizado con éxito. \n Su balance es: $ {self.update_balance}"
     
-    @property
-    def radius(self):
-        return self._radius
     
-    @radius.setter
-    def radius(self, value):
-        if value < 0:
-            raise ValueError("Radius cannot be negative")
-        self._radius = value
-
-    def __repr__(self):
-        return f"Circle(Radius:'{self._radius}')"
+    def withdraw(self,value):
+        if self.update_balance < value:
+            return f"* No hay fondos suficientes."
+        else:
+            self.update_balance -= value
+            return f"* Su retiro de $ {value} ha sido realizado con éxito. \n Su balance es: $ {self.update_balance}"
     
-
-    def area(self):
-        return math.pi * self._radius ** 2
+    def process(self,operations,*args):
+        return self.dict_operations[operations](*args)
     
-    def perimeter(self):
-        return 2 * math.pi * self._radius
-
-# Inherited class for Triangle
-class Triangle(Figure):
-    def __init__(self, side_a, side_b, side_c):
-        super().__init__("Triangle")
-        self.side_a = side_a
-        self.side_b = side_b
-        self.side_c = side_c
     
-    def area(self):
-        # Using Heron's formula to calculate the area
-        s = (self.side_a + self.side_b + self.side_c) / 2
-        return math.sqrt(s * (s - self.side_a) * (s - self.side_b) * (s - self.side_c))
-    
-    def perimeter(self):
-        return self.side_a + self.side_b + self.side_c
-
-# Inherited class for Rectangle
-class Rectangle(Figure):
-    def __init__(self, width, height):
-        super().__init__("Rectangle")
-        self.width = width
-        self.height = height
-    
-    def area(self):
-        return self.width * self.height
-    
-    def perimeter(self):
-        return 2 * (self.width + self.height)
-    
-class Person:
-  def __init__(self, name, age):
-      self.name = name
-      self.age = age
-
-  def introduce(self):
-      print(f"Hi, I'm {self.name} and I'm {self.age} years old.")
