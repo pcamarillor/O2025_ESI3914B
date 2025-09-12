5c668418b7b5695bac3bfeb33288894d6571f074
Luis Santana <angelsantana20102@outlook.com>
2025-09-12 08:13:18 -0600

    Test juniper notebook and implemented method

--- spark/notebooks/lib/luis_santana/spark_utils.py
+++ spark/notebooks/lib/luis_santana/spark_utils.py
@@ -1,6 +1,6 @@
 from urllib.parse import urlparse
 from pyspark.sql.types import StructType, StringType, IntegerType, IntegerType, ShortType, DoubleType, FloatType, BooleanType, DateType, FloatType, BooleanType, DateType, TimestampType, BinaryType, ArrayType, MapType
-
+from pyspark.sql.types import StructField
 
 
 
@@ -8,4 +8,24 @@ from pyspark.sql.types import StructType, StringType, IntegerType, IntegerType, 
 class SparkUtils:
     @staticmethod
     def generate_schema(columns_info) -> StructType:
+        types_dict = {
+            "string": StringType(),
+            "int": IntegerType(),
+            "short": ShortType(),
+            "double": DoubleType(),
+            "float": FloatType(),
+            "bollean": BooleanType(),
+            "date": DateType(),
+            "timestamp": TimestampType(),
+            "bool": BinaryType()
+        }
+        fields = []
+        for col,type in columns_info:
+            spark_type = types_dict.get(type)
+            if not spark_type:
+                raise ValueError(f"Unsupported column type: {type}")
+            fields.append(StructField(col, spark_type, True)) 
+        # print(fields)
+        schema = StructType(fields)
+        return schema
         
