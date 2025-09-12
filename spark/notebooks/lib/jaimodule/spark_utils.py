c7dd69b21212c525f9eb5131b84946795a66b588
JacbGit <127043001+JacbGit@users.noreply.github.com>
2025-09-12 08:05:41 -0600

    spark_utils

---
+++ spark/notebooks/lib/jaimodule/spark_utils.py
@@ -0,0 +1,8 @@
+from urllib.parse import urlparse
+from pyspark.sql.types import StructType, StringType, IntegerType, IntegerType, ShortType, DoubleType, FloatType, BooleanType, DateType, FloatType, BooleanType, DateType, TimestampType, BinaryType, ArrayType, MapType
+
+class SparkUtils:
+    types = {
+
+    }
+    #def generate_schema()
