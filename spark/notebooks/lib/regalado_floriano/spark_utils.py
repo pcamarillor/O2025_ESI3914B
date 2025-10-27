63d7b6964f59bf2e1b2d46d652bf0170d7b54541
Regalado <lregaladofloriano@gmail.com>
2025-10-26 20:12:14 -0600

    some utils 	modified:   spark/notebooks/lib/regalado_floriano/spark_utils.py

--- spark/notebooks/lib/regalado_floriano/spark_utils.py
+++ spark/notebooks/lib/regalado_floriano/spark_utils.py
@@ -1,6 +1,8 @@
 
 from urllib.parse import urlparse
 from pyspark.sql.types import StructField,StructType, StringType, IntegerType, IntegerType, ShortType, DoubleType, FloatType, BooleanType, DateType, FloatType, BooleanType, DateType, TimestampType, BinaryType, ArrayType, MapType
+from pyspark.sql.functions import monotonically_increasing_id
+from pyspark.sql.functions import col
 
 def parse_line(line):
     parts = line.strip().split(",")
@@ -15,12 +17,55 @@ def to_domain(url):
         host = host[4:]
     return host
 
+
+
 class SparkUtils:
     @staticmethod
     def generate_schema(columns_info) -> StructType:
         raise NotImplementedError("Not implemented yet")
 
 class SparkUtils:
+
+    @staticmethod
+    def generate_keyed_distinct_column(df):
+        """curried for ease of use"""
+        def get(col_name):
+            dist = df.select(col(col_name)).distinct()
+            return dist.withColumn('id', monotonically_increasing_id())
+        return get
+
+    @staticmethod
+    def replace_column_for_key(main_df):
+        def get_map_df(key_val_df):
+            def get_cokey(name):
+                res = main_df.alias("m").join(
+                    key_val_df.alias("n"), 
+                    col(f"n.{name}") == col(f"m.{name}"), 
+                    "left"
+                    ).drop(f"{name}").withColumnRenamed("id",f"{name}_id")
+
+                return res
+
+            return get_cokey
+        return get_map_df
+
+    @staticmethod
+    def writeToPostGres(df):
+        def get_endpoint(jdbc_url):
+            def get_name:
+                df.write \
+                .format("jdbc") \
+                .option("url", jdbc_url) \
+                .option("dbtable", df) \
+                .option("user", "postgres") \
+                .option("password", "Admin@1234") \
+                .option("driver", "org.postgresql.Driver") \
+                .save()
+            return get_name
+        return get_endpoint
+
+
+
     @staticmethod
     def generate_schema(columns_info) -> StructType:
         typeMap = {
